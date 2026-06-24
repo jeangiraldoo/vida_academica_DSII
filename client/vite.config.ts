eee8f89e1bc9f6094265ece57ed1bf81ef6e6e1a
Jean Giraldo <jeann.giraldo@gmail.com>
2026-06-23 22:50:41 -0500

    ci: add code coverage

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import istanbul from "vite-plugin-istanbul";
 import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	test: {
+		environment: "jsdom",
+		coverage: {
+			reporter: ["text", "lcov"],
+		},
+	},
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
