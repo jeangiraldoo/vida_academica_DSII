87f4d7fd5b7eae55a37f3d71ee6f9a71d3c9766e
Jean Giraldo <jeann.giraldo@gmail.com>
2026-06-29 23:16:57 -0500

    chore: implement base k8s setup

--- server/config/settings.py
+++ server/config/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-change-me-in-p
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,*" if DEBUG else "").split(",") if DEBUG else os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 INSTALLED_APPS = [
